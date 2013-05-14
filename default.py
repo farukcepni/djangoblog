b3b3377999ec0702a3e34b771bc07a533219ec56
Faruk Cepni <farukcepni@gmail.com>
2013-05-14 10:09:53 +0300

    Some fix

--- default.py
+++ default.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 def page_not_found(request):
-    return render(request, '404.html', {'message': 'Page Not Found'})
+    return render(request, '404.html', {'message': _('Page Not Found')})
 
 def server_error(request):
-    return render(request, '500.html', {'message': 'Server Error'})
+    return render(request, '500.html', {'message': _('Server Error')})
